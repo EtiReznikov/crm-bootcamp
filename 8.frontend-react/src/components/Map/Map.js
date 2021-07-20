4deec287a2313903dcdabdc2a76fde68a62361ff
eti reznikov <etirez56@gmail.com>
2021-07-20 09:28:09 +0300

    Map changes

--- 8.frontend-react/src/components/Map/Map.js
+++ 8.frontend-react/src/components/Map/Map.js
@@ -1,53 +1,29 @@
-// import React from "react"
-// import { compose, withProps } from "recompose"
-// import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-// const MyMapComponent = compose(
-//   withProps({
-//     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC0LnvgDzKrHvGI1WcBdKmQX1peUH1ODq4&&libraries=geometry,drawing,places",
-//     loadingElement: <div style={{ height: `100%` }} />,
-//     containerElement: <div style={{ height: `400px` }} />,
-//     mapElement: <div style={{ height: `100%` }} />,
-//   }),
-//   withScriptjs,
-//   withGoogleMap
-// )((props) =>
-//   <GoogleMap
-//     defaultZoom={8}
-//     defaultCenter={{ lat: -34.397, lng: 150.644 }}
-//   >
-//     {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
-//   </GoogleMap>
-// )
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
 
-// class MyFancyComponent extends React.PureComponent {
-//   state = {
-//     isMarkerShown: false,
-//   }
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: -1.2884,
+            lng: 36.8233
+          }
+        }
+      />
+    );
+  }
+}
 
-//   componentDidMount() {
-//     this.delayedShowMarker()
-//   }
-
-//   delayedShowMarker = () => {
-//     setTimeout(() => {
-//       this.setState({ isMarkerShown: true })
-//     }, 3000)
-//   }
-
-//   handleMarkerClick = () => {
-//     this.setState({ isMarkerShown: false })
-//     this.delayedShowMarker()
-//   }
-
-//   render() {
-//     return (
-//       <MyMapComponent
-//         isMarkerShown={this.state.isMarkerShown}
-//         onMarkerClick={this.handleMarkerClick}
-//       />
-//     )
-//   }
-// }
-
-// export default MyFancyComponent;
+export default GoogleApiWrapper({
+  apiKey: process.
+})(MapContainer);
